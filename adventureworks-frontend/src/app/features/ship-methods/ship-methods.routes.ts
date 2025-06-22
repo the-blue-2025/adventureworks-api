d2d36a4fb3006ace08e11b4194753fa143483327
Mona Lisa <nzhou11@example.com>
2025-06-21 23:16:24 -0230

    Cleanup

--- adventureworks-frontend/src/app/features/ship-methods/ship-methods.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ShipMethodsComponent } from './ship-methods.component';
-
-export const SHIP_METHODS_ROUTES: Routes = [
-  {
-    path: '',
-    component: ShipMethodsComponent
-  }
-]; 
--- adventureworks-frontend/src/app/shared/services/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './person.service';
-export * from './vendor.service';
-export * from './purchase-order.service';
-export * from './ship-method.service'; 
